e21d6e95f704cf1d83bb1eee72e41a85d3d5db13
kryak_vak <mr.vakashev@mail.ru>
2025-07-10 21:10:12 +0300

    user models created; alembic migrations configured

---
+++ backend/app/models/users.py
@@ -0,0 +1,51 @@
+from uuid import UUID
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.base import Base, BaseTimeStamped
+
+
+class User(BaseTimeStamped):
+    __tablename__ = 'users'
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    
+    auth_profile: Mapped["TelegramProfile"] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+    
+    token: Mapped["UserToken"] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"<User(id={self.id})>"
+
+
+class TelegramProfile(BaseTimeStamped):
+    __tablename__ = "telegram_profiles"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    telegram_id: Mapped[int] = mapped_column(unique=True)
+
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), unique=True)
+    user: Mapped["User"] = relationship(back_populates="auth_profile")
+
+    def __repr__(self):
+        return f"<TelegramProfile(id={self.id}, telegram_id={self.telegram_id})>"
+
+
+class UserToken(Base):
+    __tablename__ = "user_tokens"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    access_token: Mapped[str]
+    refresh_token: Mapped[str]
+    expires_in: Mapped[int]
+
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), unique=True)
+    user: Mapped["User"] = relationship(back_populates="token")
+    
+    def __repr__(self):
+        return f"<UserToken(id={self.id}, user_id={self.user_id}, expires_in={self.expires_in})>"
---
+++ backend/scripts/copy_migrations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Container and paths
+CONTAINER_NAME="narrow_hh-web-1"
+CONTAINER_MIGRATIONS_PATH="/backend/app/alembic/versions"
+HOST_MIGRATIONS_PATH="./app/alembic/versions"
+
+# Ensure the local folder exists
+mkdir -p "$HOST_MIGRATIONS_PATH"
+
+# Remove all local migration files to ensure full sync (optional but recommended)
+echo "Clearing local versions directory: $HOST_MIGRATIONS_PATH"
+rm -f "$HOST_MIGRATIONS_PATH"/*.py
+
+# Copy all migration files from container to host
+echo "Copying all migration files from container..."
+docker cp "$CONTAINER_NAME":"$CONTAINER_MIGRATIONS_PATH/." "$HOST_MIGRATIONS_PATH/"
+
+echo "All migration files copied successfully."
